416cc72701874b90e4dc3e19332ec769675a2a85
KotaroYoshida <y.kotaro.2091@gmail.com>
2020-11-05 16:55:06 +0900

    NowEditting->Untill Optimize!

--- TEST_FOR_ISIGHT/main.cpp
+++ TEST_FOR_ISIGHT/main.cpp
@@ -58,7 +58,7 @@ static void calcWireLine(void) {
 	obj_LL.DetermineAxies(Xi0, Eta0, Zeta0);
 }
 
-static void initializing(string InputFname) {
+static void initializing() {
 	std::cout << "Initializing...";
 	calcWireLine();
 //積分の区間数設定：スカラー
@@ -89,44 +89,17 @@ static void initializing(string InputFname) {
 	DistParams = VectorXd::Zero(3);
 //ハイパーパラメータを分ける
 	for (int i = 0; i < h_params.size() / 3; i++) {
-		AlphaParams(i) = HyperParams(i);
-		OmgEtaParams(i) = HyperParams(3 + i);
-		DistParams(i) = HyperParams(3*2 + i);
+		OmgEtaParams(i) = HyperParams(i);
+		DistParams(i) = HyperParams(3 + i);
+		AlphaParams(i) = HyperParams(i + 3 * 2);
 	}
 	fclose(fp);
 //Inputファイルから係数を読み出す
-	cout << "Reading coefficient from file...";
-	err = fopen_s(&fp, InputFname.c_str(), "r");
-	//vector<dbl> InputCoef;
-	if (err) {
-		cout << "File Open Error; " + InputFname << "\n";
-		exit(1);
-	}
-	while (fscanf_s(fp, "%lf\n", &tmp) != EOF) {
-		InputCoef.push_back(tmp);
-	}
-	if (dim != (InputCoef.size()-2) / 3) {
-		cout << "Size not match: InputCoef.size() = " << InputCoef.size() / 3 << " dim = " << dim << "\n";
-		exit(1);
-	}
-	cout << "done\n";
 //補間する対象は，出力値であるとする．
-	AlphaCoef = VectorXd::Zero(dim);
-	OmgEtaCoef = VectorXd::Zero(dim);
-	DistCoef = VectorXd::Zero(dim);
 //仮
-	for (int i = 0; i < dim; i++) {
-		AlphaCoef(i) = InputCoef[i];
-		OmgEtaCoef(i) = InputCoef[i + dim];
-		DistCoef(i) = InputCoef[i + 2 * dim];
-	}
 //初期化
-	ALPHA = VectorXd::Zero(dim);
-	OMG_ETA = VectorXd::Zero(dim);
-	DIST = VectorXd::Zero(dim);
-
-	Xi0Vec(0) = InputCoef[3 * dim];
-	Xi0Vec(1) = InputCoef[3 * dim + 1];
+	BETA = vector<dbl>(NDIV);
+	OMG_ETA = vector<dbl>(NDIV);
 	//coef->initializing
 	S = VectorXd::Zero(dim);
 	for (int i = 0; i < dim; i++) {
@@ -137,105 +110,31 @@ static void initializing(string InputFname) {
 
 static dbl RadiusBasisFunc(dbl s_i, dbl s_j, VectorXd Params) { return Params(0) * exp(-0.5 * Square(s_i - s_j) / (Params(1) * Params(1))); }
 static dbl RadiusBasisFuncSdot(dbl s, dbl s_j, VectorXd Params) { return -Params(0) * (s - s_j) * exp(-0.5 * Square(s - s_j) / Params(1) * Params(1)) / Square(Params(1)); }
-
-//点群をRBF補間するやつ => return sum_i=0^N k(s,s_i;Param)*Coef_i;
-static dbl RBF_Interpolate(dbl s, VectorXd Param, VectorXd Coef) {
-	VectorXd kernel = VectorXd::Zero(dim);
-	for (int i = 0; i < dim; i++) {
-		//kernel(i) = exp(-0.5 * (s - S(i)) * (s - S(i)) / (AlphaParams[1] * AlphaParams[1]));
-		kernel(i) = RadiusBasisFunc(s, S(i), Param);
+static void divideCoef(VectorXd a) {
+	int i;
+	for (i = 0; i < NDIV; i++) {
+		BETA[i] = a[i];
 	}
-	return Coef.dot(kernel);
+	Xi0Vec(0) = a[NDIV];
+	Xi0Vec(0) = a[NDIV+1];
+	OMG_ETA0 = 2 * atan(a[NDIV+2]) / M_PI;
+	OMG_ETA_COEF = (kappa(0) - OMG_ETA0) / (kappa(0) + OMG_ETA0);
 }
 
-static dbl alpha(dbl s) { return RBF_Interpolate(s,AlphaParams,ALPHA); }//出力をRBF補間
-static dbl omgEta(dbl s) { return RBF_Interpolate(s, OmgEtaParams, OMG_ETA); }//出力をRBF補間
-static dbl dist(dbl s) { return RBF_Interpolate(s, DistParams, DIST); }//出力をRBF補間
+static dbl IntegrandForOmgEta(dbl s) { return 2.0 * kappa(s) * beta(s); }
+static dbl IntegrateForOmgEta(dbl s) { return ScalarIntegralFunc.GaussIntegralFunc(0.0, s, bind(&IntegrandForOmgEta, _1)); }
+static dbl omgEta(dbl s) { return kappa(s) * (1 - OMG_ETA_COEF*exp(IntegrateForOmgEta(s)) / (1 + OMG_ETA_COEF*exp(IntegrateForOmgEta(s)))); }
+static dbl omgXi(dbl s) { return DiffSqrt(kappa(s), omgEta(s)); }
+static dbl omgZeta(dbl s) { return -omgXi(s) * beta(s); }
 
-static dbl alphaSdot(dbl s) {
-	VectorXd kernel = VectorXd::Zero(dim);
-	for (int i = 0; i < dim; i++) {
-		kernel(i) = RadiusBasisFuncSdot(s, S(i), AlphaParams);
-		//kernel(i) = (s - S(i)) * exp(-0.5 * Square(s - S(i)) / AlphaParams(1) * AlphaParams(1)) / Square(AlphaParams(1));
-	}
-	return ALPHA.dot(kernel);
+static Coordinates obj_L(NDIV, length_LL, omgXi,omgEta,omgZeta);
+static void initializeForCalcObj(VectorXd a) {
+	divideCoef(a);
+	beta.set_Info(Ds, BETA);
 }
-//zeta計算用
-static dbl phi(dbl s) { return acos(omgEta(s) / kappa(s)); }
-static dbl omgXi(dbl s) { return kappa(s) * sin(phi(s)); }
-static dbl omgZeta(dbl s) { return -omgXi(s) * tan(alpha(s)); }
-
-static Coordinates obj_L(NDIV, length_LL, omgXi, omgEta, omgZeta);
-//パラメータを不等式制約を考慮できるように非線形変換する．
-
-static void calcKernMatrix(VectorXd Params, MatrixXd& Mat) {
-	Mat = MatrixXd::Zero(dim, dim);
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
-			Mat(i, j) = RadiusBasisFunc(i * Ds, j * Ds, Params);
-		}
-	}
-	MatrixXd I = MatrixXd::Identity(dim, dim);
-	Mat += Params(2) * I;
-
-}
-
-#define AtanSigmoid(s) (2.0*atan(s)/M_PI)
-static dbl SingularDist(dbl s) {
-	return fabs(cos(alpha(s)) / (alphaSdot(s) + omgEta(s)));
-}
-
-static void considerIneqs() {
-	//出力は非線形変換された中身を返しているため，適切に変換してやる．
-	AlphaCoef = VectorXd::Zero(dim);
-	OmgEtaCoef = VectorXd::Zero(dim);
-	DistCoef = VectorXd::Zero(dim);
-#ifdef MY_DEBUG_MODE
-	cout << "now..." << __func__ << "\n";
-#endif
-	for (int i = 0; i < dim; i++) {
-		AlphaCoef(i) = InputCoef[i];
-		OmgEtaCoef(i) = kappa(S(i))*AtanSigmoid(InputCoef[i + dim]);
-		
-		//DistCoef(i) = InputCoef[i + 2 * dim];
-	}
-	MatrixXd KA, KE, KD;
-	calcKernMatrix(AlphaParams, KA);
-	calcKernMatrix(OmgEtaParams, KE);
-	Eigen::FullPivLU <Eigen::MatrixXd> lu_A(KA), lu_E(KE);
-	ALPHA = lu_A.solve(AlphaCoef);
-	OMG_ETA = lu_E.solve(OmgEtaCoef);
-	for (int i = 0; i < dim; i++) {
-		DistCoef(i) = SingularDist(S(i)) * Square(sin(InputCoef[i + 2 * dim]));
-	}
-	calcKernMatrix(DistParams, KD);
-	if (KD.determinant() == 0.0) {
-		KD += 1.0e-8 * MatrixXd::Identity(dim, dim);
-	}
-	FullPivLU <Eigen::MatrixXd> lu_D(KD);
-	DIST = lu_D.solve(DistCoef);
-	for (int i = 0; i < dim; i++) {
-		if (isnan(DistCoef(i))) {
-			cout << "NaN is DistCoef >>";
-			cout << "\n";
-			exit(1);
-		}
-	}
-	cout << DistCoef;
-	cout << "\n";
-	
-	//制約を加える．
-	
-	//カーネルマトリクスの設定
-	
-	
-}
-
-
-
 //目的関数の被積分関数
 static dbl objective_integrand(dbl s) {
-	Vector3d zetaSdot = omegaEta_LL(s) * obj_LL.xi(s);
+	Vector3d zetaSdot = omegaEta_LL(s) * obj_LL.xi(s) - omegaXi_LL(s) * obj_LL.eta(s);
 	if (isnan(Square(obj_LL.zeta(s).dot(obj_L.zeta(s)) - 1)) || isnan(Square(zetaSdot.dot(obj_L.xi(s)) - omgEta(s)))) {
 		cout << "isNan is found! condition>>";
 		cout << "param = " << s << ", zeta = " << obj_LL.zeta(s).dot(obj_L.zeta(s)) - 1 << ", OmgEta = " << zetaSdot.dot(obj_L.xi(s)) - omgEta(s) << "\n";
@@ -244,13 +143,11 @@ static dbl objective_integrand(dbl s) {
 	return Square(obj_LL.zeta(s).dot(obj_L.zeta(s)) - 1) + Square(zetaSdot.dot(obj_L.xi(s))-omgEta(s));
 }
 
-
-
-static dbl objective(VectorXd &a) {
+static dbl objective(int n,VectorXd &a) {
 #ifdef MY_DEBUG_MODE
 	cout << "calculate objective...";
 #endif
-	considerIneqs();
+	initializeForCalcObj(a);
 	Vector3d xi0, eta0, zeta0;
 	zeta0 = obj_LL.ZETA[0];
 	for (int i = 0; i < 2; i++) {
@@ -268,17 +165,40 @@ static dbl objective(VectorXd &a) {
 
 
 //逆行列微分のためのサブモジュールの定義
+static void calcGramMatrix(VectorXd Params, MatrixXd& Mat) {
+	Mat = MatrixXd::Zero(Kdim, Kdim);
+	for (int i = 1; i < NDIV-1; i++) {
+		for (int j = 1; j < NDIV-1; j++) {
+			Mat(i - 1, j - 1) = RadiusBasisFunc(i * Ds, j * Ds, Params);
+		}
+	}
+	Mat += MatrixXd::Identity(Kdim, Kdim) * Params[2];
+	//行列がフルランクになるまで，仮想的な誤差項を足し合わせる．
+	MatrixXd EPS = 1.0e-8 * (MatrixXd::Identity(Kdim, Kdim));
+	MatrixXd MAT_TMP = Mat;
+	for (int i = 0; i < 7; i++) {
+		FullPivLU<MatrixXd> lu_decomp(MAT_TMP);
+		int rank = lu_decomp.rank();
+		if (rank != Kdim) {
+			MAT_TMP = Mat + EPS;
+			EPS *= 10.0;
+		}
+	}
+	Mat += EPS;
+}
+
 static MatrixXd PartialKernelMatrix(int NUM,VectorXd Params) {
 	MatrixXd pK;
+	MatrixXd I = MatrixXd::Identity(Kdim, Kdim);
 	pK = MatrixXd::Identity(Kdim, Kdim);
-	if (NUM = !2) {
-		for (int i = 1; i < NDIV - 1; i++) {
-			for (int j = 1; j < NDIV - 1; j++) {
+	if (NUM != 2) {
+		for (int i = 1; i < NDIV-1; i++) {
+			for (int j = 1; j < NDIV-1; j++) {
 				switch (NUM) {
 				case 0:
-					pK(i, j) = RadiusBasisFunc(i * Ds, j * Ds, Params) / Params(0);
+					pK(i-1, j-1) = RadiusBasisFunc(i * Ds, j * Ds, Params) / Params(0);
 				case 1:
-					pK(i, j) = -(Square(i * Ds - j * Ds)) / (Square(Params(1)) * Params(1)) * RadiusBasisFunc(i * Ds, j * Ds, Params);
+					pK(i-1, j-1) = -(Square(i * Ds - j * Ds)) / (Square(Params(1))* Square(Params(1))) * RadiusBasisFunc(i * Ds, j * Ds, Params);
 				case 2:
 					//pK[i, j] = Params(0) * exp(Square(i * Ds - j * Ds) / Params(1));
 					break;
@@ -293,146 +213,73 @@ static MatrixXd PartialKernelMatrix(int NUM,VectorXd Params) {
 	return pK;
 }
 
-static void calcGramMatrix(VectorXd Params, MatrixXd &Mat) {
-	Mat = MatrixXd::Zero(Kdim, Kdim);
-	for (int i = 1; i < NDIV - 1; i++) {
-		for (int j = 1; j < NDIV - 1; j++) {
-			Mat(i-1, j-1) = RadiusBasisFunc(i * Ds, j * Ds, Params);
+static void PreCalcForConds() {
+	MatrixXd Kinv,K,tmp,K_i;
+	calcGramMatrix(AlphaParams, K);
+	Kinv = K.inverse();
+	for (int i = 0; i < 3; i++) {
+		K_i = PartialKernelMatrix(i, AlphaParams);
+		tmp = Kinv * K_i;
+		detPartDiff[i] = tmp.trace();
+		KinvPartdiff[i] = tmp * Kinv;
+	}
+	calcGramMatrix(OmgEtaParams, K);
+	Kinv = K.inverse();
+	for (int i = 0; i < 3; i++) {
+		K_i = PartialKernelMatrix(i, OmgEtaParams);
+		tmp = Kinv * K_i;
+		detPartDiff[i+3] = tmp.trace();
+		KinvPartdiff[i+3] = tmp * Kinv;
+	}
+	calcGramMatrix(DistParams, K);
+	Kinv = K.inverse();
+	for (int i = 0; i < 3; i++) {
+		K_i = PartialKernelMatrix(i, OmgEtaParams);
+		tmp = Kinv * K_i;
+		detPartDiff[i + 3*2] = tmp.trace();
+		KinvPartdiff[i + 3*2] = tmp * Kinv;
+	}
+	for (int i = 0; i < 9; i++) {
+		if (isnan(detPartDiff[i])) {
+			printf("isnan is detPartDiff-> i=%d, f = %f", i, detPartDiff[i]);
+			exit(1);
 		}
 	}
-
-}
-static dbl DeterminantParamDerivative(int NUM, VectorXd Params) {
-	MatrixXd K, Kinv, Kdot, TMP;
-	calcGramMatrix(Params, K);
-	K += Params(2) * MatrixXd::Identity(Kdim, Kdim);
-	Kdot = PartialKernelMatrix(NUM, Params);
-	Kinv = K.inverse();
-	TMP = Kinv * Kdot;
-	return K.determinant() * TMP.trace();
-
 }
 
-static MatrixXd ParticalKinverse(int NUM, VectorXd Params) {
-	MatrixXd K, Kinv, Kdot;
-	calcGramMatrix(Params, K);
-	
-	K += Params(2) * MatrixXd::Identity(Kdim, Kdim);
-	Kdot = PartialKernelMatrix(NUM, Params);
-	Kinv = K.inverse();
-	return -Kinv * Kdot * Kinv;
-}
-
-static vector<dbl> CalcConds() {
+static void CalcConds(int n, VectorXd& a, int ncond, VectorXd& COND) {
 #ifdef MY_DEBUG_MODE
 	cout << "now..." << __func__ << "\n";
 #endif
-	considerIneqs();
+	divideCoef(a);
 	vector<dbl> d;
-	dbl C[3];
-	MatrixXd KhatA, KhatE, KhatD;
-	MatrixXd I = MatrixXd::Identity(Kdim, Kdim);
-	KhatA = MatrixXd::Identity(dim, Kdim);
-	KhatE = MatrixXd::Identity(dim, Kdim);
-	KhatD = MatrixXd::Identity(dim, Kdim);
-	VectorXd AlphaCoef2 = VectorXd::Zero(dim - 2);
-	VectorXd OmgEtaCoef2 = VectorXd::Zero(dim - 2);
-	VectorXd DistCoef2 = VectorXd::Zero(dim - 2);
-#ifdef MY_DEBUG_MODE
-	cout << "Initialize Khats...";
-#endif
-/*
-	for (int i = 0; i < dim-2; i++) {
-		for (int j = 1; j < NDIV - 1; j++) {
-			KhatA(i, j-1) = RadiusBasisFunc(S(i+1), j * Ds, AlphaParams);
-			KhatE(i, j-1) = RadiusBasisFunc(S(i+1), j * Ds, OmgEtaParams);
-			KhatD(i, j-1) = RadiusBasisFunc(S(i+1), j * Ds, DistParams);
-		}
+	VectorXd A, E;
+	A = VectorXd::Zero(NDIV - 2);
+	E = VectorXd::Zero(NDIV - 2);
+	for (int i = 1; i < NDIV - 1; i++) {
+		A[i - 1] = atan(BETA[i]);
+		E[i - 1] = OMG_ETA[i];
 	}
-	KhatA += AlphaParams(2) * I;
-	KhatE += OmgEtaParams(2) * I;
-	KhatD += DistParams(2) * I;
-*/
 #ifdef MY_DEBUG_MODE
-	cout << "all done\n";
+	cout << "done\n";
 #endif
-	MatrixXd KA, KE, KD;
-#ifdef MY_DEBUG_MODE
-	cout << "calculate GramMatrix...";
-#endif
-	for (int i = 0; i < dim - 2; i++) {
-		AlphaCoef2(i) = ALPHA(i + 1);//AlphaCoef(i + 1);
-		OmgEtaCoef2(i) = OMG_ETA(i + 1);//OmgEtaCoef(i + 1);
-		DistCoef2(i) = DIST(i + 1);//DistCoef(i + 1);
-	}
-	calcGramMatrix(AlphaParams, KA);
-	calcGramMatrix(OmgEtaParams, KE);
-	calcGramMatrix(DistParams, KD);
-	dbl DET[3];
-	DET[0] = KA.determinant();
-	DET[1] = KE.determinant();
-	DET[2] = KD.determinant();
-#ifdef MY_DEBUG_MODE
-	cout << "all done\n";
-	cout << "PushBacks and calculate PartialKernel....";
-#endif
-
 	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (DET[j] == 0.0) {
-				DET[j] = 1.0e-6;
-			}
-		}
-		//C[0] = -0.5 * DeterminantParamDerivative(i, AlphaParams) / DET[0] - 0.5 * AlphaCoef.dot(KhatA * ParticalKinverse(i, AlphaParams) * KhatA.transpose() * AlphaCoef);
-		//C[1] = -0.5 * DeterminantParamDerivative(i, OmgEtaParams) / DET[1] - 0.5 * OmgEtaCoef.dot(KhatE * ParticalKinverse(i, OmgEtaParams) * KhatE.transpose() * OmgEtaCoef);
-		//C[2] = -0.5 * DeterminantParamDerivative(i, DistParams) / DET[2] - 0.5 * DistCoef.dot(KhatD * ParticalKinverse(i, DistParams) * KhatD.transpose() * DistCoef);
-		C[0] = -0.5 * DeterminantParamDerivative(i, AlphaParams) / DET[0] + 0.5 * AlphaCoef2.dot(PartialKernelMatrix(i, AlphaParams) * AlphaCoef2);
-		C[1] = -0.5 * DeterminantParamDerivative(i, OmgEtaParams) / DET[1] + 0.5 * OmgEtaCoef2.dot(PartialKernelMatrix(i, OmgEtaParams) * OmgEtaCoef2);
-		C[2] = -0.5 * DeterminantParamDerivative(i, DistParams) / DET[2] + 0.5 * DistCoef2.dot(PartialKernelMatrix(i, DistParams) * DistCoef2);
-		for (int p = 0; p < 3; p++) {
-			d.push_back(C[p]);
-			if (isnan(C[p])) {
-				switch (p) {
-				case 0:
-					cout << "isNaN is alpha" << "\n";
-					cout << DeterminantParamDerivative(i, AlphaParams) << ", "<<AlphaCoef2.dot(PartialKernelMatrix(i, AlphaParams) * AlphaCoef2) << "\n";
-					cout << "Mat\n";
-					cout << PartialKernelMatrix(i, AlphaParams);
-					
-					exit(1);
-				case 1:
-					cout << "isNaN is omgEta" << "\n";
-					cout << DeterminantParamDerivative(i, OmgEtaParams) << "\n";
-					cout << PartialKernelMatrix(i, OmgEtaParams);
-					exit(1);
-				case 2:
-					cout << "isNaN is Dist" << "\n";
-					cout << DeterminantParamDerivative(i, DistParams) << "\n";
-					cout << PartialKernelMatrix(i, DistParams);
-					cout << "coef\n";
-					cout << DIST;
-					exit(1);
-				default:
-					cout << "Wrong p num;p=" << p << "\n";
-					exit(1);
-				}
-
-			}
+		d.push_back(detPartDiff[i] - A.dot(KinvPartdiff[i] * A));
+	}
+	for (int i = 0; i < 3; i++) {
+		d.push_back(detPartDiff[i + 3] - E.dot(KinvPartdiff[i + 3] * E));
+	}
+	for (int i = 0; i < d.size(); i++) {
+		if (isnan(d[i])) {
+			printf("isnan is Detected -> i = %d, f=%f", i, d[i]);
+			exit(1);
 		}
 	}
-#ifdef MY_DEBUG_MODE
-	cout << "all done\n";
-#endif
-	d.push_back(DistCoef[0]);
-	d.push_back(DistCoef(DistCoef.size() - 1));
-	return d;
-#ifdef MY_DEBUG_MODE
-	cout << "all done\n";
-#endif
+		
 }
 
 static dbl DistMax(dbl s) {
-	return cos(alpha(s)) / (alphaSdot(s) + omgEta(s));
+	//return cos(atan(beta(s)) / (alphaSdot(s) + omgEta(s));
 }
 
 static vector<dbl> calcIneqs() {
@@ -448,44 +295,25 @@ static vector<dbl> calcIneqs() {
 
 int main(int argc, char** argv)
 {
-#ifdef MY_DEBUG_MODE
-	ofstream FILE;
-	FILE.open("INPUT.txt");
-	for (int i = 0; i < dim*3; i++) {
-		//FILE.write("%lf ", i * 0.01);
-		FILE << i * 0.0001;
-		FILE << "\n";
-	}
-	FILE << 0.01 << "\n";
-	FILE << 0.2;
-	FILE.close();
-#endif
-	string INPUT_FILENAME;
-	bool FILE_MATCH = TRUE;
-	INPUT_FILENAME = string(argv[1]);
-	if (INPUT_FILENAME != "INPUT.txt") {
-		cout << "Wrong File Name: Check Input file name and argument" << "\n";
-		exit(1);
-	}
-	initializing(INPUT_FILENAME);
+	initializing();
 
 	ofstream OBJ, COND, INEQ;
 	OBJ.open("objfunc.txt");
 	COND.open("conds.txt");
 	INEQ.open("ineqs.txt");
+	PreCalcForConds();
 	vector<dbl> c, i;
-	c = CalcConds();
-	i = calcIneqs();
-	OBJ << objective(AlphaCoef);
-	for (int p = 0; p < c.size(); p++) {
+	VectorXd CONDS;
+	coef = VectorXd::Zero(NCOORD);
+	CalcConds(NCOORD,coef,NCOND,CONDS);
+	for (int p = 0; p < 6; p++) {
 		//COND.write("%lf", c[p]);
-		COND << c[p];
-		if (p != c.size()) COND << ",";
+		COND << CONDS[p];
+		if (p != 6) COND << ",";
 	}
-	for (int j = 0; j < i.size(); j++) {
-		//INEQ.write("%lf", i[j]);
-		INEQ << i[j];
-		if (j != i.size()) INEQ << ",";
-	}
+	COND.close();
+	i = calcIneqs();
+	OBJ << objective(NCOORD,coef);
+	
 	
 }
--- TEST_FOR_ISIGHT/simparams.h
+++ TEST_FOR_ISIGHT/simparams.h
@@ -7,31 +7,33 @@
 #define NDIV 251
 static const int vbase = 251;//基定関数の個数
 constexpr int dim = vbase;
+#define NCOORD NDIV+3
 #define U_NCOORD vbase
 #define Xi_NCOORD vbase
 #define Eta_NCOORD vbase
 #define Zeta_NCOORD vbase
 constexpr int NCOORD_PER_SURFACE = 3 * vbase + 5;
 #define n_dim NCOORD_PER_SURFACE//係数ベクトルの総和の次元
-#define NCOND 3//等式制約の個数なければ1で，COND=0を加えておく
+#define NCOND 6//等式制約の個数なければ1で，COND=0を加えておく
 #define NINEQ 42//不等式制約の個数なければ1で，INEQ=-1を加えておく
 constexpr int bar_dim = 30;//バリア関数における評価点の個数
 static VectorXd AlphaParams;
 static VectorXd OmgEtaParams;
 static VectorXd DistParams;
-static VectorXd Coef;
-static VectorXd AlphaCoef, OmgEtaCoef, DistCoef;//読み込み関数用
-static VectorXd ALPHA, OMG_ETA, DIST;//ヒルベルト空間用
+static VectorXd coef;
+static dbl OMG_ETA0, OMG_ETA_COEF;
+static vector<dbl> BETA, OMG_ETA, DIST;//ヒルベルト空間用
 static VectorXd S;
 static Vector2d Xi0Vec;
-static vector<dbl> InputCoef;
-//static vector <VectorXd, Eigen::aligned_allocator<Eigen::VectorXd>> kernel;
+static vector<dbl> detPartDiff(9);
+static vector<MatrixXd, Eigen::aligned_allocator<MatrixXd>> KinvPartdiff(9);
 
 static dbl length_LL = 1.076991;//ワイヤー長さ
 static dbl Ds = length_LL / (dbl)(NDIV - 1);//刻み幅
 static GaussIntegral<Vector3d> VecIntergalFunc;//ベクトル関数積分器関数
 static GaussIntegral<dbl> ScalarIntegralFunc;//スカラー関数積分器関数
 static VectorFunction PosFuncL_str,PosFuncU_str;//曲線の位置ベクトルの線形補完関数
+static ScalarFunction beta;
 /*
 > Ritz法であらわされる設計変数群
 */
